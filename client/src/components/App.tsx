937f7a3cf424a6bc1397a6620f3b74d8eb690683
enwask <enwask@ucf.edu>
2025-03-06 10:54:30 -0500

    Styling + signup form

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,9 +1,12 @@
+import Signup from "./Signup"
+
 function App() {
     return (
         <>
             <div className="hero h-full flex">
                 <div className="hero-content w-full text-center m-auto flex-col">
                     <h1 className="text-5xl font-bold flex-row mb-8">Escavalon</h1>
+                    <Signup></Signup>
                 </div>
             </div>
         </>
