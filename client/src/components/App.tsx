ec1bbc984f8c41b93aef18c9f8599a8ad4ccacf4
enwask <enwask@ucf.edu>
2025-03-06 10:54:09 -0500

    React app setup

---
+++ client/src/components/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+    return (
+        <>
+            <div className="hero h-full flex">
+                <div className="hero-content w-full text-center m-auto flex-col">
+                    <h1 className="text-5xl font-bold flex-row mb-8">Escavalon</h1>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default App
