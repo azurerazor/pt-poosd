3b5155d5045804751625c65aa8328149b3e46e67
enwask <enwask@ucf.edu>
2025-03-06 15:17:50 -0500

    Add pretty buttons

--- client/src/components/forms/FormCard.tsx
+++ client/src/components/forms/FormCard.tsx
@@ -6,8 +6,8 @@ interface Props extends PropsWithChildren {
 
 const FormCard: React.FC<Props> = ({ children }) => {
     return (
-        <div className="card w-full max-w-sm shrink-0 shadow-2xl bg-base-100 border-2 border-base-300 p-2">
-            <div className="card-body">
+        <div className="card w-full max-w-sm shrink-0 shadow-2xl bg-base-100 border-2 border-accent p-2">
+            <div className="card-body flex flex-col items-center">
                 {children}
             </div>
         </div>
--- client/src/components/forms/Submit.tsx
+++ client/src/components/forms/Submit.tsx
@@ -4,13 +4,15 @@ interface Props extends PropsWithChildren {
     value: string;
 }
 
-const Submit: React.FC<Props> = ({ value, children }) => {
+const Submit: React.FC<Props> = ({ value }) => {
     return (
-        <input
-            type="submit"
-            value={value}
-            className="btn btn-primary w-full mt-4"
-        />
+        <span className="btn w-2/3 mt-4 h-auto p-0 deco deco-accent">
+            <input
+                type="submit"
+                value={value}
+                className="btn-deco p-1 w-full h-full text-lg serif font-semibold text-accent bg-paper-darker"
+            />
+        </span>
     )
 }
 export default Submit;
