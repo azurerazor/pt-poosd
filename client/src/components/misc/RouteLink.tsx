e413cdf2fce872d87a3062facd4075a4ec091888
enwask <enwask@ucf.edu>
2025-03-13 17:54:07 -0400

    Link -> RouteLink, add RouteButton

---
+++ client/src/components/misc/RouteLink.tsx
@@ -0,0 +1,12 @@
+import { PropsWithChildren } from 'react';
+import { Link } from 'react-router';
+
+interface Props extends PropsWithChildren { to: string; }
+
+const RouteLink: React.FC<Props> = ({ to, children }) => {
+    return (
+        <Link to={to} className="link link-hover text-primary">{children}</Link>
+    )
+}
+
+export default RouteLink;
