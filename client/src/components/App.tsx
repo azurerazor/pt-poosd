5b55d1dd32eb0c8612cb365999e09d1f4d5f3048
enwask <enwask@ucf.edu>
2025-03-13 17:53:52 -0400

    Switch to DOM router

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,15 +1,29 @@
-import Signup from "./Signup"
+import { BrowserRouter, Route, Routes } from 'react-router'
+import Login from './pages/Login'
+import NotFound from './pages/NotFound'
+import Signup from './pages/Signup'
 
 function App() {
     return (
-        <>
-            <div className="hero h-full flex">
-                <div className="hero-content w-full text-center m-auto flex-col">
-                    <h1 className="text-5xl font-bold flex-row mb-8">Escavalon</h1>
-                    <Signup></Signup>
-                </div>
-            </div>
-        </>
+        <BrowserRouter>
+            <Routes>
+                {/* Main (non-game) pages */}
+                <Route path='/' Component={Signup} />
+                <Route path='/signup' Component={Signup} />
+                <Route path='/login' Component={Login} />
+
+                {/* 404 */}
+                <Route path='/*' Component={NotFound} />
+            </Routes>
+        </BrowserRouter>
+        // <>
+        //     <div className="hero h-full flex">
+        //         <div className="hero-content w-full text-center m-auto flex-col">
+        //             <h1 className="text-5xl font-bold flex-row mb-8">Escavalon</h1>
+        //             <Signup></Signup>
+        //         </div>
+        //     </div>
+        // </>
     )
 }
 
