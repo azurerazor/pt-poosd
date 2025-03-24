82ce9cd21935bf336ee1522f764a43f1bfec4e77
enwask <enwask@ucf.edu>
2025-03-24 13:59:16 -0400

    Set remote URL

--- client/src/util/api.ts
+++ client/src/util/api.ts
@@ -1,3 +1,3 @@
 export const API_URL = (process.env.NODE_ENV == 'development')
     ? 'http://localhost:5050'
-    : 'http://localhost:5050'; // FIXME: Replace with remote URL
+    : 'http://64.225.17.164:5050';
