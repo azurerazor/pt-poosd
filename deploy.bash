8843479a10c9600d1335b929c04ef499ed0cb331
enwask <enwask@ucf.edu>
2025-03-26 14:13:01 -0400

    Add deploy script logging

--- deploy.bash
+++ deploy.bash
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 # Build the client
+echo "Building client..."
 cd client
 npm i
 npm run build
 
 # Copy over to web root + restart the nginx service
+echo "Redeploying client..."
 rm -rf /var/www/dist/
 cp -r dist/ /var/www/
 service nginx restart
 
 # Build the server
+echo "Building server..."
 cd ../server
 npm run build
 
@@ -20,5 +23,6 @@ rm -rf /var/server/
 cp -r server/ /var/
 
 # Restart the server process
+echo "Redeploying server..."
 cd /var/server
 pm2 start
