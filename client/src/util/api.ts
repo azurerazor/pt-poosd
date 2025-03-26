46854106d07943dc559710ac3b87afe19fb42589
enwask <enwask@ucf.edu>
2025-03-26 16:20:12 +0000

    Add deploy script + fix server IP

--- client/src/util/api.ts
+++ client/src/util/api.ts
@@ -1,3 +1,3 @@
 export const API_URL = (process.env.NODE_ENV == 'development')
     ? 'http://localhost:5050'
-    : 'http://64.225.17.164:5050';
+    : 'http://45.55.60.192:5050';
--- deploy.bash
+++ deploy.bash
@@ -6,5 +6,19 @@ npm i
 npm run build
 
 # Copy over to web root + restart the nginx service
+rm -rf /var/www/dist/
 cp -r dist/ /var/www/
 service nginx restart
+
+# Build the server
+cd ../server
+npm run build
+
+# Copy over the server source
+cd ../
+rm -rf /var/server/
+cp -r server/ /var/
+
+# Restart the server process
+cd /var/server
+pm2 start
