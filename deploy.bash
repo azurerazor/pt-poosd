3263a6c138631843fe8bc426e67d5b432637f901
enwask <enwask@ucf.edu>
2025-03-31 13:02:11 -0400

    Fix deploy script

--- deploy.bash
+++ deploy.bash
@@ -22,6 +22,10 @@ cd ../
 rm -rf /var/server/
 cp -r server/ /var/
 
+# Copy over the shared source
+rm -rf /var/shared/
+cp -r shared/ /var/
+
 # Restart the server process
 echo "Redeploying server..."
 cd /var/server
