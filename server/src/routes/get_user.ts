752c2bf9ed4bdac085895865370c2100bb76a638
enwask <enwask@ucf.edu>
2025-03-31 12:24:03 -0400

    Add get_user endpoint

---
+++ server/src/routes/get_user.ts
@@ -0,0 +1,17 @@
+import { Request, Response, Router } from 'express';
+import { requireAuth } from '../middleware/auth.js';
+
+// Set up the Express router
+const router = Router();
+router.use(requireAuth);
+
+// Returns the current user's name
+router.get('/', async (_: Request, res: Response) => {
+    // Fetch the current user's name and return it
+    const user = res.locals.user;
+    res
+        .status(200)
+        .json({ "username": user });
+});
+
+export default router;
