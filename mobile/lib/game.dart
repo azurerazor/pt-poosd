ea4b844bb53b50c5ca8553d9772da9ce2fef5ce0
aga <calkowska.aga@gmail.com>
2025-04-12 22:23:19 -0400

    Added ending game screen

--- mobile/lib/game.dart
+++ mobile/lib/game.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile/escavalon_material.dart';
@@ -68,6 +71,9 @@ class _GamePageContentState extends State<_GamePageContent> {
   int gamePhase = 0; // 0: start, 1: quests, 2: assassinate, 3: end
   List<Team?> questResults = List<Team?>.generate(5, (int idx) => null, growable: false);
 
+  Future<bool>? _gameSavedSuccessfully;
+  bool _gameSaved = false;
+
   @override
   Widget build(BuildContext context) {
     switch (gamePhase) {
@@ -83,9 +89,14 @@ class _GamePageContentState extends State<_GamePageContent> {
             gamePhase = 3;
           });
         }
+        // TODO: implement Merlin assassination
         return Text("Assassinating Merlin...");
       case 3:
-        return Text("Ending game...");
+
+
+
+
+        return endGame(context);
       default:
         throw ErrorDescription("Invalid game phase: $gamePhase");
     }
@@ -98,18 +109,132 @@ class _GamePageContentState extends State<_GamePageContent> {
     // TODO: run quest and return results
   }
 
-  void endGame(BuildContext context) {
-    // TODO: implement Merlin assassination
-    if (numVictories[Team.good] == 3) {
-
+  Widget endGame(BuildContext context) {
+    if (globalToken != null) {
+      _gameSavedSuccessfully = trySave();
     }
 
-    // TODO: send game results to server
-    // time started, winner, numPlayers, special roles used, winners of each round
-    // remember to send time as UTC!
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        EscavalonCard(
+          child: Text(
+            "Game Over!\nVictory for:\n${winner == Team.good ? "Good" : "Evil"}",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          )
+        ),
 
-    // return to home screen
-    Navigator.of(context).pop();
+        // save game status
+        Builder(
+          builder: (context) {
+            if (globalToken != null) {
+              return saveStatus(context);
+            } else {
+              return const SizedBox.shrink();
+            }
+          }
+        ),
+
+        EscavalonButton(
+          text: "Return to Home Screen",
+          onPressed: () {
+            bool returnToHome = true;
+
+            if (globalToken != null && _gameSaved == false) {
+              showDialog(
+                context: context, 
+                builder: (context) => AlertDialog(
+                  title: const Text("Warning!"),
+                  content: const Text("Game not saved. Are you sure you want to return to the home screen?"),
+                  actions: <Widget>[
+                    TextButton(
+                      child: const Text("NO"),
+                      onPressed: () {
+                        returnToHome = false;
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                    TextButton(
+                      child: const Text("YES"),
+                      onPressed: () {
+                        returnToHome = true;
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                  ],
+                ),
+              );
+            }
+
+            if (returnToHome) {
+              Navigator.of(context).pop();
+            }
+          },
+        ),
+      // 
+      ]
+    );
+  }
+
+  FutureBuilder<bool> saveStatus(BuildContext context) {
+    return FutureBuilder<bool>(
+      future: _gameSavedSuccessfully,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Row(
+            children: const <Widget>[
+              Expanded(
+                child: Text(
+                  "Saving game...",
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              CircularProgressIndicator(),
+            ],
+          );
+        } else if (snapshot.hasError) {
+          return Row(
+            children: [
+              Expanded(
+                child: Text(
+                  "Failed to save game: ${snapshot.error}",
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Icon(Icons.close),
+            ],
+          );
+        } else if (snapshot.hasData && snapshot.data == true) {
+          return Row(
+            children: [
+              Expanded(
+                child: Text(
+                  "Game saved successfully!",
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Icon(Icons.check),
+            ],
+          );
+        } else {
+          return Row(
+            children: [
+              Expanded(
+                child: Text(
+                  "Failed to save game.",
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Icon(Icons.close),
+            ],
+          );        
+        }
+      },
+    );
   }
 }
 
@@ -129,20 +254,16 @@ class _NightState extends State<_Night> {
   @override
   Widget build(BuildContext context) {
     switch (nightPhase) {
-      case 0: // TODO: reveal evil roles to Merlin (check for Mordred)
+      case 0: // TODO: reveal evil roles to eachother (check for Oberon)
 
 
-        if (globalRolesSelected["Merlin"] == true) {
-          setState(() {
-            nightPhase = 1;
-          });
-        } else {
-          widget.updateGamePhase(1);
-        }
-      case 1: // TODO: reveal Merlin/Morgana to Percival
+        setState(() {
+          nightPhase = 1;
+        });
+      case 1: // TODO: reveal evil roles to Merlin (check for Mordred)
 
 
-      case 2: // TODO: reveal evil roles to eachother (check for Oberon)
+      case 2: // TODO: reveal Merlin/Morgana to Percival
 
       default:
         throw ErrorDescription("Invalid night phase: $nightPhase");
@@ -153,3 +274,23 @@ class _NightState extends State<_Night> {
   
 }
 
+// TODO: send game results to server
+// time started, winner, numPlayers, special roles used (probably just as 'RoleName':'True/False'), whether each round suceeded/failed
+// remember to send time as UTC!
+Future<bool> trySave() async {
+  return true;
+  // final HttpResponse response;
+
+  // if (response.statusCode == 201) {
+  //   return true;
+  // } else {
+  //   final dynamic responseBody = jsonDecode(response.body);
+  //   if (responseBody is Map<String, dynamic>) {
+  //     final String errorMessage = responseBody['message'] ?? "Unknown error";
+  //     throw Exception(errorMessage);
+  //   } else {
+  //     throw Exception("Unknown error");
+  //   }
+  // }
+
+}
