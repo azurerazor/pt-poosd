2c1c9543e892e37ba144cbb268b80cae0dfed3cc
enwask <enwask@ucf.edu>
2025-04-15 10:53:29 -0400

    Fix socket CORS origins

--- server/src/app.ts
+++ server/src/app.ts
@@ -18,8 +18,15 @@ const PORT = process.env.PORT!;
 const MONGO_URI = process.env.MONGO_URI!;
 const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN!;
 
+// Callback for allowed CORS origins
 const CLIENT_ORIGIN_PATTERN = `https?:\\/\\/(?:\\w+.)*${CLIENT_ORIGIN}(?::\\d+)?`;
 const CLIENT_ORIGIN_REGEX = new RegExp(CLIENT_ORIGIN_PATTERN);
+function checkOrigin(origin: string | undefined, callback: (err: Error | null, origin?: string) => void) {
+    if (origin && CLIENT_ORIGIN_REGEX.test(origin)) {
+        return callback(null, origin);
+    }
+    callback(null, 'none');
+}
 
 // Connect to the MongoDB Atlas database
 mongoose
@@ -30,15 +37,7 @@ mongoose
 // Set up the Express app
 const app = express();
 app.use(cors({
-    origin: (origin, next) => {
-        // Allow requests from the client origin
-        if (origin && CLIENT_ORIGIN_REGEX.test(origin)) {
-            return next(null, origin);
-        }
-
-        // Otherwise reject
-        next(null, 'none');
-    },
+    origin: checkOrigin,
     credentials: true
 }));
 app.use(cookieParser());
@@ -48,7 +47,7 @@ app.use(express.json());
 const httpServer = http.createServer(app);
 const ioServer = new Server(httpServer, {
     cors: {
-        origin: process.env.CLIENT_ORIGIN,
+        origin: checkOrigin,
         methods: ["GET", "POST"],
         credentials: true
     }
