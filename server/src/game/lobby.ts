8825a266b587ef27e4e9319cdaab1e45a99ccf40
enwask <enwask@ucf.edu>
2025-04-14 21:35:10 -0400

    Use set for ready players instead of map

--- server/src/game/lobby.ts
+++ server/src/game/lobby.ts
@@ -2,7 +2,7 @@ import { Lobby } from "@common/game/state";
 import { deleteLobby } from "./lobbies";
 
 export class ServerLobby extends Lobby {
-    private readonly readyMap: Map<string, boolean> = new Map();
+    private readonly readySet: Set<string> = new Set();
     private onReadyCallback: (lobby: ServerLobby) => void = () => { };
 
     constructor(id: string, host: string) {
@@ -15,12 +15,12 @@ export class ServerLobby extends Lobby {
      * If all connected players are ready, triggers the onReady callback.
      */
     public setReady(username: string): void {
-        this.readyMap.set(username, true);
+        this.readySet.add(username);
         
         // Check if all players are ready (or disconnected)
         for (const player of this.getPlayers()) {
             if (!player.isConnected) continue;
-            if (!this.readyMap.get(player.username)) {
+            if (!this.readySet.has(player.username)) {
                 return;
             }
         }
@@ -28,7 +28,7 @@ export class ServerLobby extends Lobby {
         // All players are ready
         this.onReadyCallback(this);
         this.onReadyCallback = () => { }; // Reset the callback
-        this.readyMap.clear(); // Clear the ready map
+        this.readySet.clear(); // Clear the ready map
     }
 
     /**
@@ -45,7 +45,6 @@ export class ServerLobby extends Lobby {
      * Clears the ready map; does NOT reset the callback.
      */
     public clearReady(): void {
-        this.onReadyCallback = () => { };
-        this.readyMap.clear();
+        this.readySet.clear();
     }
 }
