ea3df214bf76a516deab7a70b5f84e0f89c9853c
enwask <enwask@ucf.edu>
2025-04-14 21:58:45 -0400

    Add ServerLobby.send + vote/mission maps

--- server/src/game/lobby.ts
+++ server/src/game/lobby.ts
@@ -1,12 +1,39 @@
 import { Lobby } from "@common/game/state";
 import { deleteLobby } from "./lobbies";
+import { GameEvent } from "@common/game/events";
+import { ServerEventBroker } from "./events";
 
 export class ServerLobby extends Lobby {
+    /**
+     * Set of clients that have replied ready following the
+     * most recent update event.
+     */
     private readonly readySet: Set<string> = new Set();
+
+    /**
+     * Callback that's run when all players are ready.
+     */
     private onReadyCallback: (lobby: ServerLobby) => void = () => { };
+
+    /**
+     * Map of players to their vote for the current team proposal
+     */
+    private readonly voteMap: Map<string, boolean> = new Map();
+
+    /**
+     * Map of players to pass/fail for the current mission
+     */
+    private readonly missionMap: Map<string, boolean> = new Map();
 
     constructor(id: string, host: string) {
         super(id, host, () => { deleteLobby(id); });
+    }
+
+    /**
+     * Sends an event to this lobby.
+     */
+    public send<T extends GameEvent>(event: T): void {
+        ServerEventBroker.getInstance().sendTo(this, event);
     }
 
     /**
@@ -16,7 +43,7 @@ export class ServerLobby extends Lobby {
      */
     public setReady(username: string): void {
         this.readySet.add(username);
-        
+
         // Check if all players are ready (or disconnected)
         for (const player of this.getPlayers()) {
             if (!player.isConnected) continue;
@@ -46,5 +73,58 @@ export class ServerLobby extends Lobby {
      */
     public clearReady(): void {
         this.readySet.clear();
+    }
+
+    /**
+     * Sets a player's vote for the current team proposal.
+     */
+    public setVote(username: string, vote: boolean): void {
+        this.voteMap.set(username, vote);
+    }
+
+    /**
+     * Checks whether the current team vote is passing.
+     * 
+     * Omitted votes are counted as abstentions.
+     */
+    public isVotePassing(): boolean {
+        const votes = [...this.voteMap.values()];
+
+        const yay = votes.filter(v => v).length;
+        const nay = votes.length - yay;
+
+        return yay > nay;
+    }
+
+    /**
+     * Clears the vote map.
+     */
+    public clearVotes(): void {
+        this.voteMap.clear();
+    }
+
+    /**
+     * Sets a player's pass/fail for the current mission.
+     */
+    public setMissionChoice(username: string, pass: boolean): void {
+        this.missionMap.set(username, pass);
+    }
+
+    /**
+     * Gets the number of fails for the current mission.
+     * 
+     * Missing choices are counted as passes.
+     */
+    public getNumFails(): number {
+        const votes = [...this.missionMap.values()];
+
+        return votes.filter(v => !v).length;
+    }
+
+    /**
+     * Clears the mission pass/fail map.
+     */
+    public clearMissionChoices(): void {
+        this.missionMap.clear();
     }
 }
