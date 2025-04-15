94ca2530b9b65f8064709af6d5a4c045923113fe
Brian G <rex3663@gmail.com>
2025-04-15 13:23:09 -0400

    random bullshit GO

--- client/src/components/ui/SuccessFailCard.tsx
+++ client/src/components/ui/SuccessFailCard.tsx
@@ -58,9 +58,9 @@ const SuccessFailCard: React.FC<Props> = ({ player, players, setSuccessFail}) =>
 
     return (
         <div className="card bg-base-100 shadow-sm">
-        <div className="card-body w-fit join-vertical">
-            <h1 className="text-xl font-bold flex-row">Pass or Fail this mission:</h1>
-            <div className="join join-horizontal flex justify-between space-x-5">
+        <div className="card-body w-auto join-vertical">
+            <h1 className="text-xl font-bold justify-center">Pass or Fail this mission:</h1>
+            <div className="join join-horizontal flex-row justify-between space-x-5">
                 <img src={successCard} alt="Success Card" 
                 className={`w-60 border-6 ${leftActive ? 'border-blue-400' : 'border-transparent'}`}
                 onClick={handleLeft}
