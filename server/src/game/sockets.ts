6c9df4fe51a154822b2421e8af70b4f2e7f1dba8
enwask <enwask@ucf.edu>
2025-04-15 21:45:09 -0400

    add cats!!!

--- server/src/game/sockets.ts
+++ server/src/game/sockets.ts
@@ -6,6 +6,7 @@ import { AUTH_KEY } from '../routes/auth';
 import { ServerEventBroker } from './events';
 import { getActiveLobby, getLobbyById, setActiveLobby } from './lobbies';
 import { ServerLobby } from './lobby';
+import { acquireCat } from 'networking/cats';
 
 /**
  * Map of usernames to their sockets
@@ -89,7 +90,7 @@ export function initializeSockets(server: Server): void {
         }
     });
 
-    io!.on('connection', (socket: Socket) => {
+    io!.on('connection', async (socket: Socket) => {
         const user: string = socket.data.username;
         console.log(`User ${user} opened a socket connection`);
 
@@ -174,6 +175,10 @@ export function initializeSockets(server: Server): void {
 
             // If not, add them to the lobby
             lobby.addPlayer(user);
+
+            // Assign the player a new cat image
+            const cat = await acquireCat();
+            lobby.getPlayer(user)!.avatar = cat;
         }
 
         // Update the player order
