ff1f1e900e94d5c87ef1489154a3218acaf2c945
Andy Phan <andytphan@outlook.com>
2025-04-18 10:14:43 -0400

    remove events for now and enable eslint pre-commit hook

--- common/util/random.ts
+++ common/util/random.ts
@@ -2,13 +2,13 @@
  * Randomly shuffles an array
  * Source: https://stackoverflow.com/a/2450976
  */
-export function shuffle(array: any[]) {
+export function shuffle(array: unknown[]) {
   let currentIndex = array.length;
 
   // While there remain elements to shuffle...
   while (currentIndex != 0) {
     // Pick a remaining element...
-    let randomIndex = Math.floor(Math.random() * currentIndex);
+    const randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex--;
 
     // And swap it with the current element.
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,9 +2,6 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-import json from "@eslint/json";
-import markdown from "@eslint/markdown";
-import css from "@eslint/css";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
