84eea85c4625bd2c870ba9ac8476c37c00b09e0a
Andy Phan <andytphan@outlook.com>
2025-04-20 11:24:55 -0400

    add vite-plugin-checker to catch type errors on hot-reload

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,8 +2,17 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import checker from "vite-plugin-checker";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    tsconfigPaths(),
+    checker({
+      // e.g. use TypeScript check
+      typescript: { tsconfigPath: "./tsconfig.app.json" },
+    }),
+  ],
 });
